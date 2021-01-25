b807f8f1f63a02c3d14847e3833e4cb58e0a34b1
Justine Leon Uro <justineuro@gmail.com>
2021-01-25 08:29:44 +0800

    20210125 updates
    Signed-off-by: Justine Leon Uro <justineuro@gmail.com>

--- src/main/assets/js/genQA-bs.js
+++ src/main/assets/js/genQA-bs.js
@@ -11,14 +11,14 @@ function genQ(ind,data) {
 		document.getElementById("next").remove();
 		document.getElementById("endInfo").innerHTML = '<center><h4>*** End of Quiz! ***</h4><center><br><h4>Use the <a href="#"><b>Q#</b></a> button above to return to a previous number of the quiz or the button below to create a new quiz.</h4><button type="button" class="btn btn-primary" onclick="location.reload()"><b>Create new quiz</b></button>';
 	};
-	if (i < dat.length) { // if not last item remove div endInfo; added to allow review 
-		document.getElementById("endInfo").remove();
+	if (i < dat.length) { 
+		document.getElementById("endInfo").remove(); // if not last item remove div endInfo; added to allow review
+		var question = dat[i][0];
+		qn = i + 1;
+		document.getElementById("qa").innerHTML = '<div id="question"><a id="qlabel" href="#"><b><u>Question</u>:</b></a><br></div><br><div id="answer"><a id="alabel" href="#" onclick="genA(i,dat); i +=  1;"><b><u>Answer</u>:</b></a><br></div><br><br><button id="next" type="button" class="btn btn-primary" onclick="genQ(i,dat);">Next Question</button>';
+		document.getElementById("question").innerHTML +=  '<b>' + qn + '. </b>' + question;
+		document.getElementById("end").innerHTML += '<div id="endInfo"></div>';
 	}
-	var question = dat[i][0];
-	qn = i + 1;
-	document.getElementById("qa").innerHTML = '<div id="question"><a id="qlabel" href="#"><b><u>Question</u>:</b></a><br></div><br><div id="answer"><a id="alabel" href="#" onclick="genA(i,dat); i +=  1;"><b><u>Answer</u>:</b></a><br></div><br><br><button id="next" type="button" class="btn btn-primary" onclick="genQ(i,dat);">Next Question</button>';
-	document.getElementById("question").innerHTML +=  '<b>' + qn + '. </b>' + question;
-	document.getElementById("end").innerHTML += '<div id="endInfo"></div>';
 }
 
 /* function for generating Answer link and preventive measure for additional clicks that 
